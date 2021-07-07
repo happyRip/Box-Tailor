2ec6425d9f4b91ad8e8171b1f557dfce6bc1d738
happyRip <aborbzbk@gmail.com>
2021-07-07 14:49:28 +0200

    Export triad

--- box/utility/utility.go
+++ box/utility/utility.go
@@ -1,20 +1,20 @@
 package utility
 
-type triad struct {
+type Triad struct {
 	x, y, z float64
 }
 
-func NewTriad() triad {
-	return triad{}
+func NewTriad() Triad {
+	return Triad{}
 }
 
-func (t *triad) SetValues(x, y, z float64) error {
+func (t *Triad) SetValues(x, y, z float64) error {
 	t.x = x
 	t.y = y
 	t.z = z
 	return nil
 }
 
-func (t triad) GetValues() (float64, float64, float64) {
+func (t Triad) GetValues() (float64, float64, float64) {
 	return t.x, t.y, t.x
 }
