00c0c1b0139d3137b3f12c616b80ffa15c7de903
happyRip <aborbzbk@gmail.com>
2021-07-29 11:48:19 +0200

    Fix box utility

--- box/utility/utility.go
+++ box/utility/utility.go
@@ -45,6 +45,18 @@ func (t Triad) GetValues() (float64, float64, float64, error) {
 	return t.x, t.y, t.x, nil
 }
 
+func (t Triad) X() float64 {
+	return t.x
+}
+
+func (t Triad) Y() float64 {
+	return t.y
+}
+
+func (t Triad) Z() float64 {
+	return t.z
+}
+
 type Pair struct {
 	x, y float64
 }
--- go.mod
+++ go.mod
@@ -1,4 +1,3 @@
 module github.com/happyRip/Box-Tailor
 
 go 1.16
-
